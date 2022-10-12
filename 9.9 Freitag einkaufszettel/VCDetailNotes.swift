fd164652a85e11db441585dfe955b5406e6c265a
christianeichfeld <christiandevelopment1987@gmail.com>
2022-10-12 15:27:05 +0200

    hkjg

--- 9.9 Freitag einkaufszettel/VCDetailNotes.swift
+++ 9.9 Freitag einkaufszettel/VCDetailNotes.swift
@@ -6,18 +6,114 @@
 //
 
 import UIKit
+protocol CellProductDelegate {
+    func editProduct (cell : CellProduct)
+}
 
 class CellProduct : UITableViewCell {
     
+    var cellDelegate : CellProductDelegate?
     
     @IBOutlet weak var amount: UILabel!
     @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
     @IBAction func buttonEdit(_ sender: UIButton) {
-    
+        cellDelegate?.editProduct(cell: self)
     }
 }
 
-
+extension VCDetailNotes : CellProductDelegate {
+                    func editProduct(cell: CellProduct) {
+        
+        if let indexPath = tableView.indexPath(for: cell){
+                
+            let list = self.detailList.list?.allObjects as! [Product]
+            
+            let product = list[indexPath.row]
+            
+                let alert = UIAlertController(
+                    title: "Bearbeiten", message: "Bitte bearbeite deinen Text !", preferredStyle: .alert
+                )
+                
+                alert.addTextField { field in
+                    field.placeholder = product.name
+                    field.returnKeyType = .continue
+                }
+                alert.addTextField { field in
+                    field.placeholder = product.amount.description
+                    field.returnKeyType = .continue
+                }
+                
+                alert.addTextField { field in
+                    field.placeholder = product.type
+                    field.returnKeyType = .continue
+                }
+                
+                alert.addAction(UIAlertAction(title: "Abbrechen", style: .cancel , handler: nil))
+                alert.addAction(UIAlertAction(title: "Speichern", style: .default , handler: {_ in
+                    
+                    guard let fields = alert.textFields, fields.count == 3 else {
+                        return
+                    }
+                    
+                    let nameField = fields[0]
+                    guard let titel = nameField.text
+                    else {
+                        print ("Invalid entries")
+                        return
+                    }
+                    
+                    let amountField = fields[1]
+                    guard let amount = amountField.text
+                    else {
+                        print ("Invalid entries")
+                        return
+                    }
+                    
+                    let typeField = fields[2]
+                    guard let type = typeField.text
+                    else {
+                        print ("Invalid entries")
+                        return
+                    }
+                    
+                    if titel != "" {
+                        product.name = titel
+                    }
+                    
+                    if amount != "" {
+                        product.amount = Float(amount) ?? 0
+                    }
+                    
+                    if type != "" {
+                        product.type = type
+                    }
+                    
+                   // let newProduct = Product (context: self.context)
+                    //     newProduct.name = titel
+                    //     newProduct.amount = amount
+                     //    newProduct.check = false
+                     //    newProduct.type = type
+                      //   newProduct.shoppinglist = self.detailList
+                    
+                    do{
+                        try self.context.save()
+                    }
+                    catch{
+                        print ("data fehler")
+                    }
+                    
+                    
+                    self.tableView.reloadData()
+                    
+                }
+                ))
+                present (alert,animated: true)
+            
+ 
+        }
+    }
+}
 
 class VCDetailNotes: UIViewController {
     
@@ -58,31 +154,20 @@ class VCDetailNotes: UIViewController {
             }
             
             let nameField = fields[0]
-            guard let titel = nameField.text
-            else {
-                print ("Invalid entries")
-                return
-            }
+            let titel = nameField.text
+            
             
             let amountField = fields[1]
-            guard let amount = Float(amountField.text ?? "0")
-            else {
-                print ("Invalid entries")
-                return
-            }
+             let amount = Float(amountField.text ?? "0")
             
             let typeField = fields[2]
-            guard let type = typeField.text
-            else {
-                print ("Invalid entries")
-                return
-            }
+             let type = typeField.text
             
             
             
             let newProduct = Product (context: self.context)
                  newProduct.name = titel
-                 newProduct.amount = amount
+                 newProduct.amount = amount ?? 0
                  newProduct.check = false
                  newProduct.type = type
                  newProduct.shoppinglist = self.detailList
@@ -93,16 +178,6 @@ class VCDetailNotes: UIViewController {
             catch{
                 print ("data fehler")
             }
-            
-            // let newProduct = Product (
-             //   name: titel ,
-              //  pieces: amount,
-               // check: false
-                //)
-            
-            
-            
-            // self.detailList.list.insert(newProduct, at: 0)
             
             self.tableView.reloadData()
             
@@ -140,10 +215,17 @@ extension VCDetailNotes : UITableViewDataSource,UITableViewDelegate {
         
         let cell = tableView.dequeueReusableCell (withIdentifier: "ProductCell", for: indexPath) as! CellProduct
         
-        cell.amount.text = product.amount.description
+        if product.amount > 0 {
+            cell.amount.text =  product.amount.description
+        }else {
+            cell.amount.text =  ""
+        }
         
         cell.productLabel.text = product.name
         
+        cell.typeLabel.text = product.type
+        
+        cell.cellDelegate = self
         return cell
         
     }
@@ -162,9 +244,18 @@ extension VCDetailNotes : UITableViewDataSource,UITableViewDelegate {
         
         if editingStyle == .delete{
             
-            var list = self.detailList.list?.allObjects as! [Product]
-
-            list.remove(at:indexPath.row)
+            let list = self.detailList.list?.allObjects as! [Product]
+            
+            let productToRemove = list[indexPath.row]
+            context.delete(productToRemove)
+            
+            do{
+                try self.context.save()
+            }
+            
+            catch{
+                print ("productToRemove fehler")
+            }
             
             tableView.deleteRows(at: [indexPath], with: .fade)
             
--- 9.9 Freitag einkaufszettel/VCOverViewNotes.swift
+++ 9.9 Freitag einkaufszettel/VCOverViewNotes.swift
@@ -66,12 +66,25 @@ class VCOverViewNotes: UIViewController {
     }
     
     
-    
+    // var screenSize: CGRect!
+    //  var screenWidth: CGFloat!
+    //  var screenHeight: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
-        collectionView.dataSource = self
+           collectionView.dataSource = self
+        
+        //  screenSize = UIScreen.main.bounds
+        //      screenWidth = screenSize.width
+        //     screenHeight = screenSize.height
+        
+        //let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        //      layout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+        //      layout.itemSize = CGSize(width: screenWidth/3, height: screenWidth/3)
+        //       layout.minimumInteritemSpacing = 0
+        //      layout.minimumLineSpacing = 0
+        //      collectionView!.collectionViewLayout = layout
         
         NotificationCenter.default.addObserver(self, selector: #selector(updateList), name: NSNotification.Name.init("de.shoppingList.update"), object: nil)
         fetchLists()
